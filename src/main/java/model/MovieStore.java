7dae375a5a3ee2804f92b7f2b4dd706c11ca49da
Bora Guven <bora.guven@gm.com>
2018-11-29 19:27:50 -0500

    has find by with duplication

--- src/main/java/model/MovieStore.java
+++ src/main/java/model/MovieStore.java
@@ -3,19 +3,25 @@ package model;
 import java.util.LinkedList;
 import java.util.List;
 
-import static java.util.Arrays.asList;
-
 public class MovieStore {
+
     List<Movie> movies = new LinkedList<Movie>();
+
 
     public List<Movie> findByPartialTitle(final String partialTitle) {
 
         List<Movie> results = new LinkedList<Movie>();
-        return findBy(movie -> movie.getTitle().toUpperCase().contains(partialTitle.toUpperCase()), results);
+        for (Movie movie : movies) {
+            if (movie.getTitle().toUpperCase().contains(partialTitle.toUpperCase())) {
+                results.add(movie);
+            }
+        }
+        return results;
     }
 
 
     public void add(Movie movie) {
+
         movies.add(movie);
     }
 
@@ -23,29 +29,25 @@ public class MovieStore {
     public List<Movie> findByDirector(final String director) {
 
         List<Movie> results = new LinkedList<Movie>();
-        return findBy(movie -> movie.getDirector().equals(director), results);
-    }
-
-
-
-
-
-    public List<Movie> findByReleaseYear(final int from, final int to) {
-
-        List<Movie> results = new LinkedList<Movie>();
-        return findBy(movie -> movie.getReleaseYear() > from && movie.getReleaseYear()<to, results);
-    }
-
-    interface Predicate{
-        boolean matches(Movie movie);
-    }
-    public List<Movie> findBy(Predicate predicate, List<Movie> results) {
-
-        for (Movie movie : movies){
-            if(predicate.matches(movie)){
+        for (Movie movie : movies) {
+            if (movie.getDirector().toUpperCase().equals(director.toUpperCase())) {
                 results.add(movie);
             }
         }
         return results;
     }
+
+
+    public List<Movie> findByReleaseYear(final int from, final int to) {
+
+        List<Movie> results = new LinkedList<Movie>();
+
+        for (Movie movie : movies) {
+            if (movie.getReleaseYear() > from && movie.getReleaseYear() < to) {
+                results.add(movie);
+            }
+        }
+        return results;
+    }
+
 }
